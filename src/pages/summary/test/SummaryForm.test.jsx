dba052d5b62a998ebde02bf1bc3ece2e5df389b9
oungo <y2k6273@naver.com>
2023-01-24 15:11:22 +0900

    42. `fireEvent` 를 `userEvent`로 교체하기

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("Initial conditions", () => {
   render(<SummaryForm />);
@@ -12,7 +13,9 @@ test("Initial conditions", () => {
   expect(button).toBeDisabled();
 });
 
-test("Checkbox enables button on first click and disables on second click", () => {
+test("Checkbox enables button on first click and disables on second click", async () => {
+  const user = userEvent.setup();
+
   render(<SummaryForm />);
 
   const checkbox = screen.getByRole("checkbox", {
@@ -20,8 +23,18 @@ test("Checkbox enables button on first click and disables on second click", () =
   });
   const button = screen.getByRole("button", { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(button).toBeEnabled();
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(button).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  const user = userEvent.setup();
+
+  // popover starts out hidden
+
+  // popover appears on mouseover of checkbox label
+
+  // popover disappears when we mouse out
 });
