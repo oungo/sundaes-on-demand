2b7518674ad1e7cd1ec67d46c02e3b497764a308
oungo <y2k6273@naver.com>
2023-01-24 15:19:40 +0900

    44. 테스트 요소가 페이지에 없음: 팝오버 테스트 시작하기

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -31,10 +31,21 @@ test("Checkbox enables button on first click and disables on second click", asyn
 
 test("popover responds to hover", async () => {
   const user = userEvent.setup();
+  render(<SummaryForm />);
 
   // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
 
   // popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
 
   // popover disappears when we mouse out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
